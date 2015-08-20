abdfd6d4b75ea11117dfef92e43b20907fa7218d
ashleykennington <ashley.kennington@fabricorindustries.com>
2015-08-20 21:01:39 +0800

    Complete Submission
    Completed functions ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caches a matrix to test against and then caches the invert of the test matrix
 
-## Write a short comment describing this function
+## set matrix0.tovert to x
+## set matrix0.inverted to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrix0.toinvert <<- x
+    matrix0.inverted <<- NULL
 }
 
-
-## Write a short comment describing this function
+## If matrix.toinvert is equal to matrix x then invert x
+## and set matrix0.inverted equal to invert of x
+## If matrix0.toinvert is not equal to x then set it to matrix.toinvert
+## and set matrix0.inverted to invert of x
+## return matrix0.inverted
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (identical(matrix0.toinvert, x) && is.null(matrix0.inverted)) {
+        matrix0.inverted <<- solve(x)
+    } else if (!identical(matrix0.toinvert, x)) {
+        matrix0.toinvert <<- x
+        matrix0.inverted <<- solve(x)
+    }
+    matrix0.inverted
 }
